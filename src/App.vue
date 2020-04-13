2000379e8cc670d24b6b1c796691f6974c411cad
SilentDepth <silentdepth@outlook.com>
2020-04-13 23:36:26 +0800

    change font config

--- src/App.vue
+++ src/App.vue
@@ -27,3 +27,17 @@
 </script>
 
 <style lang="scss" src="./assets/css/base.scss"></style>
+
+<style>
+  @import url('/font/inter.css');
+
+  #app {
+    font-family: Inter, theme('fontFamily.sans');
+  }
+
+  @supports (font-variation-settings: normal) {
+    #app {
+      font-family: 'Inter var', theme('fontFamily.sans');
+    }
+  }
+</style>
